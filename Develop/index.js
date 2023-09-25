400dcf310e0859b9410527eb3ea25434bc9a24ba
lexabexa <lexabexa3@gmail.com>
2023-09-24 20:17:02 -0700

    js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generatedMarkdown");
+const fs = require("fs");
+const path = require ("path");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+//const questions = [];
+const questions = [
+    {
+      type: "input",
+      name: "Title",
+      message: "Project name.",
+    }, {
+        type: "input",
+        name: "Table Of Contents",
+        message: "List README file contents.",
+      },
+    {
+      type: "input",
+      name: "Description",
+      message: "Describe the purpose of project.",
+    },
+    {
+      type: "input",
+      name: "Screenshot",
+      message: "Post path to image here."
+    },
+    {
+        type: "input",
+        name: "Link",
+        message: "URL of deployed application."
+      },
+      {
+        type: "input",
+        name: "Installation",
+        message: "Necessary Software"
+      },
+   {
+      type: "input",
+      name: "Usage",
+      message: "What languages or technologies are associated with this project?",
+    },
+    {
+      type: "input",
+      name: "Tests",
+      message: "Required tests if applicable.",
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "References?."
+      },
+      {
+        type: "input",
+        name: "Questions",
+        message: "Your Github link and contact info"
+      },
+      {
+        type: "checkbox",
+        name: "License",
+        message: "Which license?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "none"],
+      },    
+  ];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+//function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
+//function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+      console.log("Creating README.md File");
+      writeToFile("./test/Readme", generateMarkdown({ ...responses }));
+    });
+}
 init();
--- Develop/utils/generatedMarkdown.js
+++ Develop/utils/generatedMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !=="none") {
+    return
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,8 +16,43 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+//function generateMarkdown(data) {
+//return `# ${data.title}
+//`;
+//}
+//module.exports = generateMarkdown;
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
+# Table of Contents
+* [Title](#Title)
+* [Table of Contents](#Table of Contents)
+* [Description](#Description)
+* [Screenshot](#Screenshot)
+* [Link](#Link)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Tests](#Tests)
+* [Contributing](#Contributing)
+* [Questions](#Questions)
+* [License](#License)
+# Description
+${data.Description}
+# Screenshot
+![alt-text](${data.Screenshot})
+# Link
+${data.link}
+# Installation
+${data.Installation}
+# Usage
+${data.Usage}
+# Tests
+${data.Tests}
+# Contributing
+${data.Contributing}
+# Questions
+${data.Questions}
+# License
+${renderLicenseBadge(data.License)}
 
 `;
 }
