ff977149d7ee03fa4a7184edb91a8503ad5f3a4a
lexabexa <lexabexa3@gmail.com>
2023-09-28 03:51:12 -0700

    R-r

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-const FS = Require('fs');
-const generateMarkdown = Require('./utils/generatedMarkdown');
-const { default: inquirer } = Require('inquirer');
+const FS = require('fs');
+const generateMarkdown = require('./utils/generatedMarkdown');
+const { default: inquirer } = require('inquirer');
 
 // TODO: Create an array of questions for user input
 //const questions = [];
